9ece2f94d3e22cd911f5beb12099891ff1d8ea39
DEVUSAIK2 <saikumar.trainings@gmail.com>
2017-07-20 17:29:41 +0530

    Update First.java

--- eclipse-workspace/Sample/src/First.java
+++ eclipse-workspace/Sample/src/First.java
@@ -3,6 +3,6 @@ public class First {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
        System.out.println("Hello");
-       System.out.println("Bye123");
+       System.out.println("Bye1243");
    }
 }
